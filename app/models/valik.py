d8b005da3c3d6d29f8e03c1ad9aa43f4611fb129
Valenook <valikvalik14@yandex.ru>
2017-05-13 14:06:59 +0300

    added relations

--- app/models/valik.py
+++ app/models/valik.py
@@ -15,17 +15,59 @@ from sqlalchemy.ext.declarative import declarative_base
 #!
 #engine = create_engine('sqlite:///:memory:', echo=True)
 #!
-Base = declarative_base()
+#step_up child -> parent
+#step_down parent -> child
 
-class branch(Base):
+Base = declarative_base()
+class Team(Base):
+    __tablename__ = 'Teams'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    step_down = relationship("Projects", back_populates="step_up")
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+ #   def __repr__(self):
+
+class Project(Base):
+    __tablename__ = 'Projects'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    team_id = Column(Integer, ForeignKey('Teams.id'))
+    step_up = relationship("Teams", back_populates="step_down")
+    step_down1 = relationship("Branches", back_populates="step_up")
+    step_down2 = relationship("Users", back_populates="step_up")
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+ #   def __repr__(self):
+     
+class User(Base):
+    __tablename__ = 'Users'
+    id = Column(Integer, primary_key=True)
+    nickname = Column(String)
+    password = Column(String)
+    email = Column(String)
+    joined = Column(DataTime)
+    project_id = Column(Integer, ForeignKey('Projects.id'))
+    status = Column(String)
+    rating = Column(Integer)
+    step_up = relationship("Projects", back_populates="step_down2")
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+ #   def __repr__(self):
+     
+class Branch(Base):
     __tablename__ = 'Branches'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    project_id = Column(Integer)
+    project_id = Column(Integer, ForeignKey("Projects.id"))
     created = Column(DateTime)
     status = Column(Integer)
     price_all = Column(Integer)
-    step_down = relationship("Tasks", back_populates="Branches")
+    step_up = relationship("Projects", back_populates="step_down1")
+    step_down = relationship("Tasks", back_populates="step_up")
     def __init__(self, id, name, project_id, created, status, price_all):
         self.id = id
         self.name = name
@@ -35,7 +77,7 @@ class branch(Base):
         self.price_all = price_all
  #   def __repr__(self):
  #       return "<Branch('%s','%s', '%s', '%s')>" % (self.name, self.created, self.status, self.price_all)
-class task(Base):
+class Task(Base):
     __tablename__ = 'Tasks'
     id = Column(Integer, primary_key=True)
     title = Column(String)
@@ -46,8 +88,8 @@ class task(Base):
     tags = Column(String)
     status = Column(Integer)
     price = Column(Integer)
-    branch_id = Column(Integer)
-    step_up = relationship("Branches", back_populates="Tasks")
+    branch_id = Column(Integer, ForeignKey("Branches.id"))
+    step_up = relationship("Branches", back_populates="step_down")
     def __init__(self, id, title, created, modified, author_id, executor_id, tags, status, price, branch_id):
         self.id = id
         self.title = title
@@ -61,23 +103,23 @@ class task(Base):
         self.branch_id = branch_id
    # def __repr__(self):
    #     return "<Task('%s','%s', '%s', '%s', '%s')>" % (self.title, self.created, self.modified, self.tags)
-class File(Base):
-    __tablename__ = 'Files'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    created = Column(DateTime)
-    descriprtion = Column(String)
-    author_id = Column(Integer)
-    path = Column(String)
-    task_id = Column(Integer)
-    def __init__(self, id, name, created, description, author_id, path, task_id):
-        self.id = id
-        self.name = name
-        self.created = created
-        self.description = description
-        self.author_id = author_id 
-        self.path = path
-        self.task_id = task_id
+#class File(Base):
+#    __tablename__ = 'Files'
+#    id = Column(Integer, primary_key=True)
+#    name = Column(String)
+#    created = Column(DateTime)
+#    descriprtion = Column(String)
+#    author_id = Column(Integer)
+#    path = Column(String)
+#    task_id = Column(Integer)
+#    def __init__(self, id, name, created, description, author_id, path, task_id):
+#        self.id = id
+#        self.name = name
+#        self.created = created
+#        self.description = description
+#        self.author_id = author_id 
+#        self.path = path
+#        self.task_id = task_id
    # def __repr__(self):
    #     return "<Task('%s','%s', '%s', '%s', '%s')>" % (self.title, self.created, self.modified, self.tags)
 
