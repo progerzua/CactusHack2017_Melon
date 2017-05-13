f893ce6a940d3d2575e393f10359e130138fd32a
Artem <a.progerz@gmail.com>
2017-05-13 16:13:35 +0300

    Everything fixed

--- app/controller.py
+++ app/controller.py
@@ -1,6 +1,9 @@
 from flask import render_template,session,redirect,url_for,escape,request,flash,abort
 from app import app, db
+from app.models import User, Team, Project, Branch, Task
 
 @app.route('/')
 def index():
+    team = Team("teamname")
+    project = Project("projectname")
     return "Hello, World!"
--- app/models.py
+++ app/models.py
@@ -9,29 +9,35 @@ Base = declarative_base()
 class Team(Base):
 
     __tablename__ = 'Teams'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(32), unique=True)
+
     step_down = relationship("Projects", back_populates="step_up")
-    def __init__(self, id, name):
-        self.id = id
+
+    def __init__(self, name):
         self.name = name
 
 class Project(Base):
 
     __tablename__ = 'Projects'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(32))
     team_id = Column(Integer, ForeignKey('Teams.id'))
+
     step_up = relationship("Teams", back_populates="step_down")
+
     step_down1 = relationship("Branches", back_populates="step_up")
     step_down2 = relationship("Users", back_populates="step_up")
-    def __init__(self, id, name):
-        self.id = id
+
+    def __init__(self, name):
         self.name = name
 
 class User(Base):
 
     __tablename__ = 'Users'
+
     id = Column(Integer, primary_key=True)
     nickname = Column(String(32), unique=True)
     password = Column(String(32))
@@ -40,33 +46,39 @@ class User(Base):
     project_id = Column(Integer, ForeignKey('Projects.id'))
     status = Column(String)
     rating = Column(Integer)
+
     step_up = relationship("Projects", back_populates="step_down2")
+
     step_down = relationship("Tasks", back_populates="step_up2")
-    def __init__(self, id, name):
-        self.id = id
+
+    def __init__(self, name):
         self.name = name
 
 class Branch(Base):
 
     __tablename__ = 'Branches'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(32))
     project_id = Column(Integer, ForeignKey("Projects.id"))
     created = Column(DateTime)
     status = Column(Integer)
     price_all = Column(Integer)
+
     step_up = relationship("Projects", back_populates="step_down1")
+
     step_down = relationship("Tasks", back_populates="step_up1")
-    def __init__(self, id, name, project_id, created, status, price_all):
-        self.id = id
+
+    def __init__(self, name, created, status, price_all):
         self.name = name
-        self.project_id = project_id
         self.created = created
         self.status = status
         self.price_all = price_all
 
 class Task(Base):
+
     __tablename__ = 'Tasks'
+
     id = Column(Integer, primary_key=True)
     title = Column(String(32))
     created = Column(DateTime)
@@ -77,19 +89,19 @@ class Task(Base):
     status = Column(Integer)
     price = Column(Integer)
     branch_id = Column(Integer, ForeignKey("Branches.id"))
+
     step_up1 = relationship("Branches", back_populates="step_down")
+
     step_up2 = relationship("Users", back_populates="step_down")
-    def __init__(self, id, title, created, modified, author_id, executor_id, tags, status, price, branch_id):
-        self.id = id
+
+    def __init__(self, title, created, modified, executor_id, tags, status, price):
         self.title = title
         self.created = created
         self.modified = modified
-        self.author_id = author_id
         self.executor_id = executor_id
         self.tags = tags
         self.status = status
         self.price = price
-        self.branch_id = branch_id
 
 #class File(Base):
 #    __tablename__ = 'Files'
