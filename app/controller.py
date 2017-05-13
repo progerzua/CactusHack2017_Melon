92cc99ee914e14722371f2e43e088f8ffb28aae6
Artem <a.progerz@gmail.com>
2017-05-13 23:38:11 +0300

    To sync job with Valik
    Sorry for our useless commits, it should be better later

--- app/controller.py
+++ app/controller.py
@@ -7,30 +7,9 @@ from app import app, db
 from app.models import Team, Project, User
 from sqlalchemy.orm import sessionmaker
 from flask import Flask,session, request, flash, url_for, redirect, render_template, abort ,g
-from flask.ext.login import login_user , logout_user , current_user , login_required
-
 
 @app.route('/')
 def index():
 
     if request.method == 'GET':
         return render_template('index.html')
-
-@app.route('/login',methods=['GET','POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    nickname = request.form['username']
-    password = request.form['password']
-    registered_user = User.query.filter_by(nickname=nickname,password=password).first()
-    if registered_user is None:
-        flash('Username or Password is invalid' , 'error')
-        return redirect(url_for('login'))
-    login_user(registered_user)
-    flash('Logged in successfully')
-    return redirect(request.args.get('next') or url_for('index'))
-#@app.route("/logout")
-#@login_required
-#def logout():
-#    logout_user()
-#    return redirect(somewhere)
