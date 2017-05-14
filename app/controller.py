0cc17888d2b8fb3a01c7173aa821109f5ebf8d07
Artem <a.progerz@gmail.com>
2017-05-14 05:17:50 +0300

    Middle-commit

--- app/controller.py
+++ app/controller.py
@@ -13,22 +13,23 @@ from app.forms import LoginForm
 def index():
     return render_template('landing.html')
 
-@app.route('/login', methods=(['GET', 'POST']))
+@app.route('/login', methods=(['POST']))
 def login():
-    form = LoginForm()
-    if request.method == "GET":
-        return render_template('login.html',form = form)
-    else:
-        mess = ""
-        if form.validate_on_submit():
-            user = db.session.query(User).filter_by(nickname=form.login.data).first()
-            if user:
-                if user.password == form.password.data:
-                    return redirect(url_for('index'))
-                else :
-                    mess = "Wrong password"
-            else :
-                mess = "User doesn`t exist"
-        else:
-            mess = "Validation error"
-        return render_template('login.html',form = form, message = mess)
+
+    # form = LoginForm()
+    # if request.method == "GET":
+    #     return render_template('login.html',form = form)
+    # else:
+    #     mess = ""
+    #     if form.validate_on_submit():
+    #         user = db.session.query(User).filter_by(nickname=form.login.data).first()
+    #         if user:
+    #             if user.password == form.password.data:
+    #                 return redirect(url_for('index'))
+    #             else :
+    #                 mess = "Wrong password"
+    #         else :
+    #             mess = "User doesn`t exist"
+    #     else:
+    #         mess = "Validation error"
+    #     return render_template('login.html',form = form, message = mess)
